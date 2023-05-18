c958efce7e33372a7848cfda2ef89f8aea2caa07
Riyaanadani <91545569+Riyaanadani@users.noreply.github.com>
2023-05-18 17:55:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My parents New healthy diner')
 
 streamlit.header(' Breakfast Favorites')
@@ -14,7 +18,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header("Fruityvice Fruit Advice!")
 
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -28,7 +32,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show )
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 
@@ -40,7 +44,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
-import snowflake.connector 
+#import snowflake.connector 
 
 
 
@@ -55,3 +59,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 streamlit.write('Thanks for adding ', add_my_fruit)
 my_cur.execute('insert into fruit_load_list values ('from streamlit')")
+               
+              
+               
+ 
