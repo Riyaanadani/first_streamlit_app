10f35e5afccfd78fbc4c23dabadddf191c75b548
Riyaanadani <91545569+Riyaanadani@users.noreply.github.com>
2023-05-19 11:34:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
- 
-
 streamlit.title('My Parents New Healthy Diner')
-
- 
 
 streamlit.header('Breckfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -17,28 +13,18 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
- 
-
 #import pandas
-
- 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
- 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) default selection
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
- 
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
- 
 
 #Create repeatable code block(a function)
 def get_fruityvice_data(this_fruit_choice):
@@ -47,6 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   #streamlit.write('The user entered ', fruit_choice)
@@ -57,7 +44,7 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-
+  
 #streamlit.stop()
 
  
